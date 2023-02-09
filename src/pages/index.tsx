6e21bc7d3416962765c624dce387bc21e0e78f40
Yok <yok.c@jenosize.com>
2023-02-09 18:12:44 +0700

    edit component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
 // import TakePicture from "@/components/TakePicture";
 import React, { ChangeEvent, useState } from "react";
 
-function index() {
+function Index() {
   const [image, setImage] = useState<File>();
   const [preview, setPreview] = useState<string>("");
-  // const [takePicture, setTakePicture] = useState<boolean>(false);
+
 
   console.log("image", image);
 
@@ -34,19 +34,7 @@ function index() {
           )}
         </div>
       </div>
-      {/* <div className=" col-start-2 col-span-2 row-start-4   sm:col-start-3 sm:col-span-1 sm:row-start-4 ">
-        <div className="flex justify-center ">
-          <button
-            className="bg-blue-600  py-2 w-[8rem] rounded-md text-white "
-            onClick={() => {
-              setTakePicture(true);
-            }}
-          >
-            ถ่ายรูป
-          </button>
-        </div>
-      </div> */}
-
+ 
       <div className="col-span-6 row-start-4   sm:col-span-6 sm:row-start-4">
         <div className="flex justify-center  ">
           <div className="bg-blue-600 py-2 w-[8rem] rounded-md   flex justify-center">
@@ -66,9 +54,9 @@ function index() {
       <div className="col-start-2 col-span-4 row-start-5  sm:col-start-3 sm:col-span-2 sm:row-start-5 border-dashed border-2 border-blue-600 px-2 py-2">
         response
       </div>
-      {/* {takePicture && <TakePicture />} */}
+      
     </div>
   );
 }
 
-export default index;
+export default Index;
